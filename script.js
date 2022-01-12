4b3ad916321d6a1f3ac41803f954f78e3d31c33e
pdhende <pdhendework@gmail.com>
2022-01-12 15:35:53 -0500

    Created function to genrate password, added 1st validation for length

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // 1. Prompt user to input the password length  
+    do {
+      var pwdLen = prompt("Enter the length of the password");
+      if(pwdLen < 8 || pwdLen > 128) 
+       {
+        alert("Please enter password length of at least 8 characters and no more than 128 characters.");
+       } 
+    }while(pwdLen < 8 || pwdLen > 128);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
