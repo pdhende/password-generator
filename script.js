89ed839e4ff067b8afd48ba2d44c3933563341f5
pdhende <pdhendework@gmail.com>
2022-01-14 16:53:24 -0500

    Updated comments, created arrays for character types refactored previous code

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ var pwdLen = 0;
 var uCase;
 var lCase;
 var strPwdCriteria = "";
+//Arrays containing character types
+var uCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// var numArr = [];
+// var splCharArr = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // 1. Prompt user to input the password length  
+  // 1. Function to prompt user to input the password length and validate the input
     var pwdLgth = function() {
       pwdLen = prompt("Enter the length of the password");
       if(pwdLen < 8 || pwdLen > 128) 
@@ -21,32 +26,18 @@ function generatePassword() {
         pwdLgth();
       }
 
-    //2. Prompt user if LowerCase/Uppercase/Numeric/Special Characters is required
+  //2. Function to prompt user to choose character type LowerCase/Uppercase/Numeric/Special Characters and validate the selection.
     var charTypes = function() {
       var boolLC = confirm("Do you want password to contain Lowercase?");
       if(boolLC) {
         strPwdCriteria = strPwdCriteria + "L";
       }
-      var boolUC = confirm("Do you want password to contain Uppercase?");
-      if(boolUC) {
-        strPwdCriteria = strPwdCriteria + "U";
-      }
-      var boolNum = confirm("Do you want password to contain numeric values?");
-      if(boolNum) {
-        strPwdCriteria = strPwdCriteria + "N";
-      }
-      var boolSC = confirm("Do you want password to contain special characters?");
-      if(boolSC) {
-        strPwdCriteria = strPwdCriteria + "S";
-      }
-      if(strPwdCriteria === "") {
-        alert("Please select at least one character type.");
-      }
     };
-    
+
     while(strPwdCriteria === "") {
       charTypes();
     }
+    return strPwdCriteria;
 }
 
 // Write password to the #password input
