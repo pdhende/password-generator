0f43e2c9a1550cdf3de500e34fd8bd9e585c2f6b
pdhende <pdhendework@gmail.com>
2022-01-14 20:50:46 -0500

    Updated the function charTypes

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var pwdLen = 0;
 var uCase;
 var lCase;
 var strPwdCriteria = "";
-//Arrays containing character types
+//Arrays containing character
 var uCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-// var numArr = [];
-// var splCharArr = [];
+var numArr = [0,1,2,3,4,5,6,7,8,9];
+// var splCharArr = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
+var splCharArr = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -26,11 +27,26 @@ function generatePassword() {
         pwdLgth();
       }
 
-  //2. Function to prompt user to choose character type LowerCase/Uppercase/Numeric/Special Characters and validate the selection.
+    //2. Function to prompt user to choose character type LowerCase/Uppercase/Numeric/Special Characters and validate the selection.
     var charTypes = function() {
       var boolLC = confirm("Do you want password to contain Lowercase?");
       if(boolLC) {
         strPwdCriteria = strPwdCriteria + "L";
+      }
+      var boolUC = confirm("Do you want password to contain Uppercase?");
+      if(boolUC) {
+        strPwdCriteria = strPwdCriteria + "U";
+      }
+      var boolNum = confirm("Do you want password to contain numeric values?");
+      if(boolNum) {
+        strPwdCriteria = strPwdCriteria + "N";
+      }
+      var boolSC = confirm("Do you want password to contain special characters?");
+      if(boolSC) {
+        strPwdCriteria = strPwdCriteria + "S";
+      }
+      if(strPwdCriteria === "") {
+        alert("Please select at least one character type.");
       }
     };
 
